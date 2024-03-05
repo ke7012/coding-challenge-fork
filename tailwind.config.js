3edffbe9c2b1edf4f9d18cbfe889c9cfd5109bba
Kenli <kenli.lao@hotmail.com>
2024-03-05 21:14:03 +0100

    Edit of tailwind files configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    colors: {
+      "wrong-red": "#f28482",
+      "correct-green": "#84a59d",
+    },
+    extend: {
+    },
   },
   plugins: [],
-};
+}
+
