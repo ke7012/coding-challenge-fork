d0fdd9dc9b738407a1061ce638ab39f77c1f5145
Kenli <kenli.lao@hotmail.com>
2024-03-07 20:29:11 +0100

    Added filter functionality

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,5 +1,6 @@
+import { render } from "react-dom";
 import TodoItem from "./TodoItem";
-import React from "react";
+import React, { useState } from "react";
 
 /**
  * Component that displays two separate lists: open tasks and completed tasks.
@@ -20,12 +21,15 @@ interface TodoListProps {
 }
 
 function TodoList({ todos, toggleTodo, deleteTodo }: TodoListProps) {
+  /* Keeps state of current Filter */
+  const [filter, setFilter] = useState("");
+
   /* Renders the open tasks */
   const renderTodo = () => {
     const todoList: Todo[] = todos.filter((todo) => !todo.completed);
     if (todoList.length === 0) {
       return (
-        <div className="text-gray-300 text-opacity-50 text-center text-xs ">
+        <div className="p-2 text-gray-300 text-opacity-50 text-center text-xs ">
           No To Do Tasks
         </div>
       );
@@ -50,7 +54,7 @@ function TodoList({ todos, toggleTodo, deleteTodo }: TodoListProps) {
     const completedList: Todo[] = todos.filter((todo) => todo.completed);
     if (completedList.length === 0) {
       return (
-        <div className="text-gray-300 text-opacity-50 text-center text-xs">
+        <div className="p-2 text-gray-300 text-opacity-50 text-center text-xs">
           No Completed Tasks
         </div>
       );
@@ -70,17 +74,63 @@ function TodoList({ todos, toggleTodo, deleteTodo }: TodoListProps) {
     }
   };
 
+  /* Returns completed and todo lists */
+  const renderAll = () => {
+    return (
+      <div className="grid gap-2 grid-cols-2 min-w-[600px]">
+        <div className="todoContainer items-stretch">
+          <h2 className="text-center text-lg font-bold">To Do</h2>
+          {renderTodo()}
+        </div>
+        <div className="completedContainer items-stretch">
+          <h2 className="text-center text-lg font-bold">Completed</h2>
+          {renderCompleted()}
+        </div>{" "}
+      </div>
+    );
+  };
+
+  /* Displays open, completed and all todo items based on filter */
+  const filterTodo = () => {
+    switch (filter) {
+      case "all":
+        return renderAll();
+      case "completed":
+        return (
+          <>
+            <h2 className="text-center m-2 text-lg font-bold">Completed</h2>
+            {renderCompleted()}
+          </>
+        );
+      case "open":
+        return (
+          <>
+            <h2 className="text-center m-2 text-lg font-bold">To Do</h2>
+            {renderTodo()}
+          </>
+        );
+      default:
+        return renderAll();
+    }
+  };
+
   return (
-    <div className="grid grid-cols-2 max-w-[500px]">
-      <div className="todoContainer">
-        <h2 className="text-center m-2 text-lg font-bold">To Do</h2>
-        {renderTodo()}
+    <>
+      <div className="flex justify-center items-center p-2 m-2">
+        <label htmlFor="filter">Show </label>
+        <select
+          className="rounded-lg ml-2 p-2 lg"
+          value={filter}
+          id="filter"
+          onChange={(e) => setFilter(e.target.value)}
+        >
+          <option value="all">All</option>
+          <option value="open">Open</option>
+          <option value="completed">Completed</option>
+        </select>
       </div>
-      <div className="completedContainer">
-        <h2 className="text-center m-2 text-lg font-bold">Completed</h2>
-        {renderCompleted()}
-      </div>
-    </div>
+      {filterTodo()}
+    </>
   );
 }
 
